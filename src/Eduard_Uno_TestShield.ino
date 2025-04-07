03af6f2f23f3071d00b5f3dfdb4e33cde36bbab8
FönixFogelFuchs <42917429+Fuchsfuchsfuchs@users.noreply.github.com>
2025-04-07 11:36:50 +0200

    Create Eduard_Uno_TestShield.ino

---
+++ src/Eduard_Uno_TestShield.ino
@@ -0,0 +1,20 @@
+
+#include <Adafruit_NeoPixel.h>      //Necessary library to use the Adafruit Neopixels
+#ifdef __AVR__
+ #include <avr/power.h>             // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define LED_PIN   6                 // Which pin on the Arduino is connected to the NeoPixels?
+#define LED_COUNT 5                 // How many NeoPixels are attached to the Arduino?
+
+
+
+void setup() {
+  // Alle Pins auf Input 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
